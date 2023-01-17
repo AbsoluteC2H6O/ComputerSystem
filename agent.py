3d48b82d9b624ec82b9b22bef08fbb8c7a8f85a8
alfredofp24 <alfredo.ferreira@footballpools.com>
2023-01-16 20:06:46 -0400

    First commit

---
+++ agent.py
@@ -0,0 +1,45 @@
+import numpy as np #importamos la lib de objetos de matrices multidimensionales
+
+#creamos nuestra clase TwoArmedBandit
+class TwoArmedBandit():
+
+    #iniciar variables: # de bandits = 2, alpha y reset para limpiar las variables
+    def __init__(self, alpha=1, eps=0.5):
+        self.arms = 2 #numero de bandits
+        self.alpha = alpha #valor de alpha
+        self.eps = eps #valor de epsilon
+        self.reset() #resetear
+
+    #reiniciar las variables y condiciones
+    def reset(self):
+        self.action = 0 #accion
+        self.reward = 0 #recompensa
+        self.rewards = np.zeros(self.arms)
+        self.iteration = 0 #tasa de aprendizaje
+        self.values = np.zeros(self.arms)
+
+    #valorar las acciones
+    def update(self, action, reward):
+        self.action = action
+        self.reward = reward
+        self.rewards[action] += reward
+        self.iteration += 1
+        self.values[action] = self.values[action] + self.alpha * (reward - self.values[action])
+
+    #decidir que accion tomar
+    def get_action(self, mode):
+        if mode == 'random':
+            return np.random.choice(self.arms)
+        elif mode == 'greedy':
+            return np.argmax(self.values)
+        elif mode == 'epsilon':
+            p = np.random.random()
+            if p < self.eps:
+                return np.random.choice(self.arms)
+            else:
+                return np.argmax(self.values)
+
+    #renderizar
+    def render(self):
+        print("Iteration: {}, Action: {}, Reward Actual: {}, Rewards: {}, Values: {}".format(
+            self.iteration, self.action, self.reward, self.rewards, self.values))
---
+++ main.py
@@ -0,0 +1,42 @@
+#importar modulos
+import sys #lista de directorios/carpetas
+import gym #standard API
+import gym_environments #entorno para nuestro agente
+from agent import TwoArmedBandit #importamos nuestra agente
+
+num_iterations = 100 if len(sys.argv) < 2 else int(sys.argv[1])
+version = "v0" if len(sys.argv) < 3 else sys.argv[2]
+
+env = gym.make(f"TwoArmedBandit-{version}") #entorno
+agent = TwoArmedBandit(0.1, 0.9) #agente
+
+env.reset(options={'delay': 1}) #resetear el entorno
+
+print("\nEjecutando el modo: Random", end="\n\n")
+for iteration in range(num_iterations):
+    action = agent.get_action("random") #elegir el caso de estudio: random, greedy, epsilon
+    _, reward, _, _, _ = env.step(action) #obtener la recompensa
+    agent.update(action, reward) #actualizar
+    agent.render() #nos sirve para ver la evolucion en la tabla de valores e imprimir por pantalla
+
+env.reset(options={'delay': 1})
+agent.reset()
+print("\nEjecutando el modo: Greedy", end="\n\n")
+
+for iteration in range(num_iterations):
+    action = agent.get_action("greedy")
+    _, reward, _, _, _ = env.step(action)
+    agent.update(action, reward)
+    agent.render()
+
+env.reset(options={'delay': 1})
+agent.reset()
+print("\nEjecutando el modo: Epsilon", end="\n\n")
+
+for iteration in range(num_iterations):
+    action = agent.get_action("epsilon")
+    _, reward, _, _, _ = env.step(action)
+    agent.update(action, reward)
+    agent.render()
+
+env.close() #cierre del entorno
