905ad054d2c0fea3551cdbc2454e8afd1bb61df2
alfredofp24 <alfredo.ferreira@footballpools.com>
2023-01-17 16:08:49 -0400

    Modify iterations and execute main function for the agent.

--- Task1/main.py
+++ Task1/main.py
@@ -6,40 +6,30 @@ from agent import TwoArmedBandit #importamos nuestra agente
 
 num_iterations = 100 if len(sys.argv) < 2 else int(sys.argv[1])
 version = "v0" if len(sys.argv) < 3 else sys.argv[2]
+num_methods = 3
 
 env = gym.make(f"TwoArmedBandit-{version}") #entorno
 agent = TwoArmedBandit(0.1, 0.5) #valores de alpha y epsilon
 
 env.reset(options={'delay': 1}) #resetear el entorno
 
-print("\nEjecutando el modo: Random", end="\n\n")
-for iteration in range(num_iterations):
-    action = agent.get_action("random") #elegir el caso de estudio: random, greedy, epsilon
-    _, reward, _, _, _ = env.step(action) #obtener la recompensa
-    agent.update(action, reward) #actualizar
-    agent.render() #nos sirve para ver la evolucion en la tabla de valores e imprimir por pantalla
-agent.renderRewardTotal() #para conocer la ganancia total obtenida
+def executeMethods(method, num_iterations):
+    print("\nEjecutando el modo: {}".format(method), end="\n\n")
+    for iteration in range(num_iterations):
+        action = agent.get_action(method) #elegir el caso de estudio: random, greedy, epsilon
+        _, reward, _, _, _ = env.step(action) #obtener la recompensa
+        agent.update(action, reward) #actualizar
+        agent.render() #nos sirve para ver la evolucion en la tabla de valores e imprimir por pantalla
+    agent.renderRewardTotal() #para conocer la ganancia total obtenida
+    agent.reset()
 
-env.reset(options={'delay': 1})
-agent.reset()
-print("\nEjecutando el modo: Greedy", end="\n\n")
-
-for iteration in range(num_iterations):
-    action = agent.get_action("greedy")
-    _, reward, _, _, _ = env.step(action)
-    agent.update(action, reward)
-    agent.render()
-agent.renderRewardTotal()
-
-env.reset(options={'delay': 1})
-agent.reset()
-print("\nEjecutando el modo: Epsilon", end="\n\n")
-
-for iteration in range(num_iterations):
-    action = agent.get_action("epsilon")
-    _, reward, _, _, _ = env.step(action)
-    agent.update(action, reward)
-    agent.render()
-agent.renderRewardTotal()
-
+for method_number in range(num_methods):
+    method = ''
+    if method_number == 0:
+        method ='random'
+    if method_number == 1:
+        method ='greedy'
+    if method_number == 2:
+        method ='epsilon'
+    executeMethods(method,num_iterations)
 env.close() #cierre del entorno
