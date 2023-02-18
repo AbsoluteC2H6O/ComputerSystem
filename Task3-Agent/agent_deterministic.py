e119583314bc87ffc7f4b2a1ae0d06ae412ba713
Abraham Mousalli <locutor.abraham@gmail.com>
2023-02-18 00:10:29 -0400

    Task3-Agent: Actualizacion del agente con el metodo  determinista

--- Task3-Agent/agent_deterministic.py
+++ Task3-Agent/agent_deterministic.py
@@ -1,5 +1,6 @@
 import numpy as np
-class MonteCarlo:
+
+class MonteCarloDet:
     def __init__(self, states_n, actions_n, gamma, epsilon):
         self.states_n = states_n
         self.actions_n = actions_n
@@ -10,7 +11,8 @@ class MonteCarlo:
     def reset(self):
         self.episode = [] # número de episodios
         self.q = np.zeros((self.states_n, self.actions_n)) # valorar cada par estado-acción
-        self.pi = np.full((self.states_n, self.actions_n), 1 / self.actions_n) # recibe un estado y retorna una acción, determina que acción seguir
+        # self.pi = np.zeros(self.states_n, , self.actions_n) # recibe un estado y retorna una acción, determina que acción seguir
+        self.pi = np.full((self.states_n, self.actions_n), 1 / self.actions_n)
         self.returns = np.zeros((self.states_n, self.actions_n)) # acumular los promedios
         self.returns_n = np.zeros((self.states_n, self.actions_n)) # acumula el numero de iter
 
@@ -19,7 +21,7 @@ class MonteCarlo:
         if terminated == True:
             self._update_q()
             self._update_pi()
-            self.episode = []
+            self.episode = [] # inicializo de nuevo los episodios
 
     def _update_q(self):
         states_actions = []
@@ -46,19 +48,32 @@ class MonteCarlo:
                 self.returns[state][action] / self.returns_n[state][action]
             )
 
+                    # self.q[s_t][a_t] = sum(returns[states_action]) / len(returns[states_action])
+        # for state, action in states_actions:
+        #     first_occurence = next(
+        #         i
+        #         for i, step in enumerate(self.episode)
+        #         if step[0] == state and step[1] == action
+        #     )
+        #     G = sum(
+        #         [
+        #             step[2] * (self.gamma**i)
+        #             for i, step in enumerate(self.episode[first_occurence:])
+        #         ]
+        #     )
+        #     self.returns[state][action] += G
+        #     self.returns_n[state][action] += 1
+        #     self.q[state][action] = (
+        #         self.returns[state][action] / self.returns_n[state][action]
+        #     )
+
     def _update_pi(self):
         states = []
-        [states.append(state) for state, _, _ in self.episode if state not in states]
+        [states.append(state) for state, _, _ in self.episode if state in states]
         for state in states:
             best_action = np.argmax(self.q[state])
-            for action in range(self.actions_n):
-                if action == best_action:
-                    self.pi[state][action] = (
-                        1 - self.epsilon + (self.epsilon / self.actions_n)
-                    )
-                else:
-                    self.pi[state][action] = self.epsilon / self.actions_n
-
+            self.pi[state] = best_action
+    
     def get_action(self, state):
         return np.random.choice(self.actions_n, p=self.pi[state])
 
--- Task3-Agent/main.py
+++ Task3-Agent/main.py
@@ -3,6 +3,7 @@ import time
 import gym_environments
 from gym.envs.registration import register
 from agent import MonteCarlo
+from agent_deterministic import MonteCarloDet
 
 # registro Env
 register (
@@ -15,6 +16,16 @@ register (
     entry_point="Env.v0.robot_maze:RobotMazeEnv",
 )
 
+register (
+    id="Robot-v1",
+    entry_point="Env.v1.robot_battery:RobotBatteryEnv",
+)
+
+register (
+    id="FrozenLake-v2",
+    entry_point="Env.v2.frozen_lake.frozen_lake:FrozenLakeEnv",
+)
+
 def train(env, agent, episodes):
     for _ in range(episodes):
         observation, _ = env.reset()
@@ -24,7 +35,7 @@ def train(env, agent, episodes):
             new_observation, reward, terminated, truncated, _ = env.step(action)
             agent.update(observation, action, reward, terminated)
             observation = new_observation
-
+            
 
 def play(env, agent):
     observation, _ = env.reset()
@@ -37,14 +48,24 @@ def play(env, agent):
 
 
 if __name__ == "__main__":
-    env = gym.make("RobotBattery-v0", render_mode="human")
+    env = gym.make("FrozenLake-v2", render_mode="human")
     agent = MonteCarlo(
         env.observation_space.n, env.action_space.n, gamma=0.9, epsilon=0.9
     )
 
-    train(env, agent, episodes=100)
-    agent.render()
+    agent_deterministic = MonteCarloDet(
+        env.observation_space.n, env.action_space.n, gamma=0.9, epsilon=0.9
+    )
 
-    play(env, agent)
+    mode = 'deterministic'
+    if(mode == 'deterministic'):
+        train(env, agent_deterministic, episodes=10000)
+        agent_deterministic.render()
+        play(env, agent_deterministic)
+
+    else:
+        train(env, agent, episodes=10000)
+        agent.render()
+        play(env, agent)
 
     env.close()
