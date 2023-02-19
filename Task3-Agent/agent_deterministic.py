Task3-Agent Corrected formula and condition for probabilities.

Task3-Agent/agent_deterministic.py
@@ -1,31 +1,33 @@
 import numpy as np
 
+
 class MonteCarloDet:
-    def __init__(self, states_n, actions_n, gamma, epsilon):
+    def __init__(self, states_n, actions_n, gamma):
         self.states_n = states_n
         self.actions_n = actions_n
         self.gamma = gamma
-        self.epsilon = epsilon
+        self.pi = np.full((self.states_n, self.actions_n), 1 / self.actions_n)
         self.reset()
 
     def reset(self):
-        self.episode = [] # número de episodios
-        self.q = np.zeros((self.states_n, self.actions_n)) # valorar cada par estado-acción
-        # self.pi = np.zeros(self.states_n, , self.actions_n) # recibe un estado y retorna una acción, determina que acción seguir
+        self.episode = []  # número de episodios
+        # valorar cada par estado-acción
+        self.q = np.zeros((self.states_n, self.actions_n))
         self.pi = np.full((self.states_n, self.actions_n), 1 / self.actions_n)
-        self.returns = np.full((self.states_n, self.actions_n), 0) # Acumular los promedios
-        self.returns_n = np.zeros((self.states_n, self.actions_n)) # acumula el numero de iter
+        # Acumular los promedios
+        self.returns = np.zeros((self.states_n, self.actions_n))
+        # acumula el numero de iter
+        self.returns_n = np.zeros((self.states_n, self.actions_n))
 
     def update(self, state, action, reward, terminated):
         self.episode.append((state, action, reward))
         if terminated == True:
             self._update_q()
             # self._update_pi()
-            self.episode = [] # inicializo de nuevo los episodios
+            self.episode = []  # inicializo de nuevo los episodios
 
-    def _update_q(self):       
+    def _update_q(self):
         G = 0
-        #print('episode', self.episode)
         self.episode.reverse()
         states_actions = []
         [
@@ -33,38 +35,56 @@ class MonteCarloDet:
             for state, action, _ in self.episode
             if (state, action) not in states_actions
         ]
-        #print('reverse', self.episode)
         reward = self.episode[0][2]
-        for state, action, reward_a in self.episode[1:]:
+        state_t= self.episode[0][0]
+        action_t = self.episode[0][1]
+        # for state, action in states_actions:
             # first_occurence = next(
             #     i
             #     for i, step in enumerate(self.episode)
             #     if step[0] == state and step[1] == action
             # )
-            # print('first:',first_occurence)
+        # print(self.episode)
+        for state, action, reward_a in self.episode[1:]:
             G = self.gamma*G + reward
             reward = reward_a
-            print('G', G)
-            if (state, action) in states_actions:
-                self.returns[state][action] = G
-                self.returns_n[state][action] = np.average(self.returns[state][action])
-                self.q[state][action] = self.returns_n[state][action]
-                self.pi[state] = np.argmax(self.q[state])
-            #self.pi = np.argmax(self.q[state])  
-        #     print('returns', self.returns[state][action])
-        # print('returns total', self.returns)
-        # print('q total', self.q)
+            # print('G', G)
+            
+            if (state_t, action_t) in states_actions[0:len(states_actions)-2]:
+                self.returns[state_t][action_t] = G
+                self.returns_n[state_t][action_t] = np.average(
+                    self.returns[state_t][action_t])
+                self.q[state_t][action_t] = self.returns_n[state_t][action_t]
+                # if(np.argmax(self.q[state][action])!=self.pi[0][0]):
+                print('np.argmax',np.argmax(self.q[state_t]), action)
+                self.pi[state_t] = np.argmax(self.q[state_t][action])
 
     # def _update_pi(self):
     #     states = []
-    #     [states.append(state) for state, _, _ in self.episode if state in states]
+    #     [states.append(state)
+    #      for state, _, _ in self.episode if state in states]
     #     for state in states:
-    #         best_action = np.argmax(self.q[state])
+    #         best_action = np.argmax(self.actions_n,self.q[state])
     #         self.pi[state] = best_action
-    
+
     def get_action(self, state):
-        print(self.pi[state])
-        return np.random.choice(self.actions_n, p=self.pi[state])
+        # print('q', self.q)
+        # print('pi', self.pi, state)
+        probability = 0
+        isZero = False
+        for action in self.pi[state]:
+            if action > 0:
+                probability+=action
+            else:
+                isZero = True
+        if isZero != True and probability ==1:
+            # print('p en 1',)
+            return np.random.choice(self.actions_n, p=self.pi[state])
+        else:
+            # print("p no 1")
+            return -1
+            
+        
 
     def get_best_action(self, state):
         return np.argmax(self.q[state])

Task3-Agent/main.py
@@ -4,6 +4,7 @@ import gym_environments
 from gym.envs.registration import register
 from agent import MonteCarlo
 from agent_deterministic import MonteCarloDet
+import numpy as np
 
 # registro Env
 register (
@@ -29,12 +30,17 @@ register (
 def train(env, agent, episodes):
     for _ in range(episodes):
         observation, _ = env.reset()
+        # observation = np.random.randint(0, 4)
         terminated, truncated = False, False
         while not (terminated or truncated):
             action = agent.get_action(observation) # Accion aleatoria
-            new_observation, reward, terminated, truncated, _ = env.step(action)
-            agent.update(observation, action, reward, terminated)
-            observation = new_observation
+            if action !=-1:
+                new_observation, reward, terminated, truncated, _ = env.step(action)
+                agent.update(observation, action, reward, terminated)
+                observation = new_observation
+            else:
+                terminated = True
+                
             
 
 def play(env, agent):
@@ -42,8 +48,11 @@ def play(env, agent):
     terminated, truncated = False, False
     while not (terminated or truncated):
         action = agent.get_best_action(observation)
-        observation, _, terminated, truncated, _ = env.step(action)
-        env.render()
+        if action !=-1:
+            observation, _, terminated, truncated, _ = env.step(action)
+            env.render()
+        else:
+            terminated = True
         time.sleep(1)
 
 
@@ -54,7 +63,7 @@ if __name__ == "__main__":
     )
 
     agent_deterministic = MonteCarloDet(
-        env.observation_space.n, env.action_space.n, gamma=0.9, epsilon=0.9
+        env.observation_space.n, env.action_space.n, gamma=0.9
     )
 
     mode = 'deterministic'