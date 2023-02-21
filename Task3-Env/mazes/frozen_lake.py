17ed94290d0cbce3d4769b038dea7913d699b7fc
alfredofp24 <alfredo.ferreira@footballpools.com>
2023-02-21 01:11:21 -0400

    Task3-Env corrected kruskal implementation and maze printed.

--- Task3-Env/mazes/frozen_lake.py
+++ Task3-Env/mazes/frozen_lake.py
@@ -2,11 +2,6 @@
 Frozen Lake environment as a maze
 """
 import maze_generators
-import kruskal2
-from colorama import init
-from colorama import Fore, Back, Style
-height = 11
-width = 27
 class FrozenLake:
     def __init__(self, **kwargs):
         self._rows = kwargs.get("rows", 4)
@@ -15,25 +10,9 @@ class FrozenLake:
             "maze_generator_class", maze_generators.KruskalMazeGenerator
         )(self._rows, self._cols)
         self.walls = maze_generator.generate()
-       
-    # def printMaze(self):
-    #     # Initialize colorama
-    #     init()
-    #     for i in range(0, height):
-    #         for j in range(0, width):
-    #             if (self.walls[i][j] == 'u'):
-    #                 print(Fore.WHITE + str(self.walls[i][j]), end=" ")
-    #             elif (self.walls[i][j] == ' '):
-    #                 print(Fore.GREEN + str(self.walls[i][j]), end=" ")
-    #             else:
-    #                 print(Fore.RED + str(self.walls[i][j]), end=" ")
-    #         print('\n')
-            
-    def render(self):
-        # render the top wall
-        # print('maze', self.walls)
-        print("-" * int(self._cols * 2 + 1))
 
+    def render(self):
+        print("-" * int(self._cols * 2 + 1))
         for i in range(self._rows):
             for j in range(self._cols):
                 # evaluate if there is a left wall
@@ -47,33 +26,37 @@ class FrozenLake:
 
                 # render the left wall if exists
                 if has_left_wall:
-                    # every left cell has a left wall
                     print("|", end="")
                 else:
                     # space if there is no a left wall
                     print(" ", end="")
 
                 # render the cell
-                print(" ", end="")
+                print(' ', end="")
 
             # render the right wall for the current row
             print("|")
 
+            # render the first bottom wall
+            print("-", end="")
+
             # render the bottom wall when if exists
-            for j in range(self._cols * 2 + 1):
+            for j in range(self._cols):
                 current_index = i * self._cols + j
                 bottom_index = (i + 1) * self._cols + j
                 has_bottom_wall = (
                     i == self._rows - 1
-                    or j % 2 == 0
                     or (current_index, bottom_index) in self.walls
                     or (bottom_index, current_index) in self.walls
                 )
                 if has_bottom_wall:
-                    print("-", end="")
+                   print("-", end="")
                 else:
                     # space if there is not a bottom wall
                     print(" ", end="")
+                
+                # render the next bottom wall
+                print("-", end="")
 
             # finally, end of line
             print("")
--- Task3-Env/mazes/maze_generators/KruskalMazeGenerator.py
+++ Task3-Env/mazes/maze_generators/KruskalMazeGenerator.py
@@ -1,23 +1,25 @@
 """
 Maze generator based on depth first search algorithm
 """
-from typing import List, Any, Tuple, Set
+from typing import List, Tuple, Set
 import random
 from .MazeGenerator import MazeGenerator
 
 GRAPH_SIZE = 10
-cels = [(i,j) for j in range(GRAPH_SIZE) for i in range(GRAPH_SIZE)]
-neighbors = lambda n : [(n[0]+dx,n[1]+dy) for dx,dy in ((-1,0),(1,0),(0,-1),(0,1))
-                       if n[0]+dx >= 0 and n[0]+dx < GRAPH_SIZE and n[1]+dy >= 0 and n[1]+dy < GRAPH_SIZE]
+cels = [(i, j) for j in range(GRAPH_SIZE) for i in range(GRAPH_SIZE)]
+
+
+def neighbors(n): return [(n[0]+dx, n[1]+dy) for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1))
+                          if n[0]+dx >= 0 and n[0]+dx < GRAPH_SIZE and n[1]+dy >= 0 and n[1]+dy < GRAPH_SIZE]
+
 
 class Kruskal:
     def __init__(self, cels):
         self.cel_map = {}
-        print(len(cels))
-        for i,val in enumerate(cels):
+        for i, val in enumerate(cels):
             n = self.NodeGraph(val, i)
             self.cel_map[val] = n
-            
+
     def find(self, cel):
         return self.find_cel(cel).cel
 
@@ -34,81 +36,60 @@ class Kruskal:
         cel2 = self.find_cel(node2)
         if cel1.cel != cel2.cel:
             cel1.cel = cel2
+
     class NodeGraph:
         def __init__(self, val, cel):
             self.val = val
             self.cel = cel
+
+
 class KruskalMazeGenerator(MazeGenerator):
     def __init__(
         self,
         num_rows: int,
         num_cols: int,
-        neighborhood: List[Tuple[int, int]] = [(0, -1), (1, 0), (0, 1), (-1, 0)],
+        neighborhood: List[Tuple[int, int]] = [
+            (0, -1), (1, 0), (0, 1), (-1, 0)],
     ) -> None:
         super().__init__(num_rows, num_cols, neighborhood)
-        
-    # def _init_walls(self) -> None:
-        # for i in range(self.num_rows):
-        #     for j in range(self.num_cols):
-        #         current_index = i * self.num_cols + j
-        #         for offset_i, offset_j in self.neighborhood:
-        #             n_i, n_j = i + offset_i, j + offset_j
 
-        #             if not ((0 <= n_i < self.num_rows) and (0 <= n_j < self.num_cols)):
-        #                 continue
+    def _init_walls(self) -> None:
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                current_index = i * self.num_cols + j
+                for offset_i, offset_j in self.neighborhood:
+                    n_i, n_j = i + offset_i, j + offset_j
 
-        #             neighbor_index = n_i * self.num_cols + n_j
+                    if not ((0 <= n_i < self.num_rows) and (0 <= n_j < self.num_cols)):
+                        continue
 
-        #             if (current_index, neighbor_index) in self.walls or (
-        #                 neighbor_index,
-        #                 current_index,
-        #             ) in self.walls:
-        #                 continue
+                    neighbor_index = n_i * self.num_cols + n_j
 
-        #             self.walls.add((current_index, neighbor_index))
-                    
+                    if (current_index, neighbor_index) in self.walls or (
+                        neighbor_index,
+                        current_index,
+                    ) in self.walls:
+                        continue
+
+                    self.walls.add((current_index, neighbor_index))
+
     def generate(self, start: int = 0) -> Set[Tuple[int, int]]:
-        # self._init_walls()
+        self._init_walls()
         walls = [(cel, nbor) for cel in cels for nbor in neighbors(cel)]
-        maze: Set[Tuple[int, int]] = set()
+        maze = []
         maze_kruskal = Kruskal(cels)
         while len(maze) < len(cels)-1:
-            # print(len(walls))
             wall = walls.pop(random.randint(0, len(walls)-1))
             if maze_kruskal.find(wall[0]) != maze_kruskal.find(wall[1]):
+                celZero = wall[0][0]
+                celZeroAd = wall[0][1]
+                celOne = wall[1][0]
+                celOneAd = wall[1][1]
                 maze_kruskal.union(wall[0], wall[1])
-                # if (wall[0][0],wall[0][1]) in maze:
-                #     maze.remove((wall[0][0], wall[0][1]))
-                    
-                # if (wall[0][1],wall[0][0]) in maze:
-                #     maze.remove((wall[0][1], wall[0][0]))
-                    
-                # if (wall[1][0], wall[1][1]) in maze:
-                #     maze.remove((wall[1][0], wall[1][1]))
-                # if (wall[1][1], wall[1][0]) in maze:
-                #     maze.remove((wall[1][1], wall[1][0]))
-                
-                # self.walls.remove((wall[0], wall[1]))
-                # self.walls.remove((wall[1][0], wall[0][0]))
-                # self.walls.remove((wall[0][0], wall[1][0]))
-                # if (wall[0] and wall[1]) in maze:
-                #     maze.remove((wall[0][0], wall[0][1]))
-                # if (wall[1]) in maze:
-                #     maze.remove((wall[0][1], wall[0][0]))  
-                print('wall[0][0]', wall[0][0])
-                maze.add((wall[0][0],wall[0][1]))
-                maze.add((wall[1][0],wall[1][1]))
-                # self.walls.add((wall[0][0],wall[0][1]))
-                # self.walls.add((wall[1][0],wall[1][1]))
-                # self.walls.add((wall[0][0],wall[0][1]))
-                # self.walls.add((wall[1][0],wall[1][1]))
-                                
-
-        # self.walls.remove((0,0))
-        # print(len(self.walls))
-
-        return maze
-            
-           
-
-
+                maze.append(wall)
+                if (celZero*self.num_cols + celZeroAd,celOne*self.num_cols + celOneAd) in self.walls:
+                    self.walls.remove((celZero*self.num_cols +celZeroAd,celOne*self.num_cols +celOneAd))
+                elif (celOne*self.num_cols + celOneAd,celZero*self.num_cols + celZeroAd) in self.walls:
+                    self.walls.remove((celOne*self.num_cols +celOneAd,celZero*self.num_cols +celZeroAd))
+        
+        return self.walls
