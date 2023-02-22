f0440ebbdc1a67f1633450f46744dbee0e86c83b
Abraham Mousalli <locutor.abraham@gmail.com>
2023-02-22 00:18:57 -0400

    Task3-Env Comenzar a generar la matrix P

--- Task3-Env/mazes/maze_generators/KruskalMazeGenerator.py
+++ Task3-Env/mazes/maze_generators/KruskalMazeGenerator.py
@@ -104,15 +104,14 @@ class KruskalMazeGenerator(MazeGenerator):
                 components[self.maze_kruskal.find((row, col))] = []
         key = 0
         keyComp = []
-        n=0
+        n = 0
         for row in range(self.num_rows):
             for col in range(self.num_cols):
                 components[self.maze_kruskal.find(
                     (row, col))].append((row, col))
                 key = self.maze_kruskal.find((row, col))
                 keyComp.insert(n, n)
-                n+=1
-        
+                n += 1
         # Generate init and finish character position
         initPosition = components[key][random.randint(
             0, len(components[key])-1)]
@@ -120,10 +119,35 @@ class KruskalMazeGenerator(MazeGenerator):
         while endPosition == initPosition:
             endPosition = components[key][random.randint(
                 0, len(components[key])-1)]
-        
-        stateInit = initPosition[0]* self.num_cols + initPosition[1]
-        # print('robotPositions',initPosition, endPosition,stateInit)
-        # print('init state = ', stateDic[0]*self.num_cols + stateDic[1])
+
+        stateInit = initPosition[0] * self.num_cols + initPosition[1]
+        stateFinal = endPosition[0] * self.num_cols + endPosition[1]
+        print('robotPositions',initPosition, endPosition,stateInit, stateFinal)
         dic1 = dict(zip([0, 1, 2, 3], [None]*4))
+        # print(dic1)
         dic2 = dict(zip(keyComp, [dic1]*(self.num_rows*self.num_cols)))
         # pos.append((1, 2, 0.0, False),(1, 2, 0.0, False),(1, 2, 0.0, False))
+        i = 0
+        for rows in range(self.num_rows): # 0 1
+            for cols in range(self.num_cols):
+                current_index = rows * self.num_cols + cols
+                left_index = rows * self.num_cols + cols - 1
+                down_index = (rows+1) * self.num_cols + cols
+                right_index = rows * self.num_cols + cols + 1
+                up_index = (rows-1) * self.num_cols + cols
+                #print('actual', current_index, left_index, down_index, right_index, up_index)
+                for action in range(4):
+                    if(current_index == stateFinal):
+                        dic1[action] = (1, current_index, 0.0, True)
+                        print('here')
+                    # else:
+                    #     print('')
+                    #     if (action == 0):  # movimiento en izq
+                    #         dic1[action] = (1, actual, 0.0, False), (1, 2, 0.0, False), (1, 2, 0.0, False)
+                    #     if (action == 1):  # movimiento en abajo
+                    #         dic1[action] = (1, actual, 0.0, False), (1, 2, 0.0, False), (1, 2, 0.0, False)
+                    #     if (action == 2):  # movimiento en der
+                    #         dic1[action] = (1, actual, 0.0, False), (1, 2, 0.0, False), (1, 2, 0.0, False)
+                    #     if (action == 0):  # movimiento en arriba
+                    #         dic1[action] = (1, actual, 0.0, False), (1, 2, 0.0, False), (1, 2, 0.0, False)
+        print(dic2)
